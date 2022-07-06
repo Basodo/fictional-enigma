dcccfa1eb93f036c14eb7e5ebc495160d84747a7
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2022-07-06 20:01:32 +0200

    Add files via upload

---
+++ enigma.py
@@ -0,0 +1,14 @@
+#main file. vanuit hier worden alle losse functies aangeroepen
+
+from lamps import translate
+from caesar_encrypt import caesar_cipher
+from keyboard import vercijfer
+from filter import filter
+from plugboard import plugtest
+
+
+vercijfer()
+filter()
+#caesar_cipher(3)
+plugtest()
+translate()
