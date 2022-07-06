f513281ef4260f042b131e35c86ea90c48a6c67a
Basodo <105501797+Basodo@users.noreply.github.com>
2022-07-06 09:58:48 -0700

    Update filter.py

--- filter.py
+++ filter.py
@@ -2,8 +2,13 @@
 
 from keyboard import num_list
 
+
 def filter():
+    detected = False
     for x in range(len(num_list)):
-        if (num_list[x] <= 0 and num_list[x] != -64)or num_list[x]>26:
-            print("the message contains characters that can't be encrypted")
-            
+        if detected == False:
+            if (num_list[x] <= 0 and num_list[x] != -64)or num_list[x] > 26:
+                print("the message contains characters that can't be encrypted")
+                detected = True
+        else:
+            quit()
