10038e5f85c2ce41a145ed18ca6ae4d4f921d4cc
Basodo <105501797+Basodo@users.noreply.github.com>
2022-07-11 21:38:34 +0200

    Add files via upload

---
+++ rotors.py
@@ -0,0 +1,10 @@
+from keyboard import num_list
+from keyboard import vercijfer
+from keyboard import volgordestring
+
+def volgorde():
+    tets = volgordestring[1]
+    print(tets)
+    
+    def rotor1():
+        hello = cool
