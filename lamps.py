5d78b71e4a5f4307816707c8c856c7a029938c1d
Teiz3 <108479294+Teiz3@users.noreply.github.com>
2022-09-02 11:17:05 +0200

    Delete lamps.py

--- lamps.py
+++
@@ -1,9 +0,0 @@
-#lamps is de output functie die num_list weer omzet in letters
-
-from keyboard import num_list
-
-def translate():
-     secret_message = ("")
-     for x in range(len(num_list)):
-          secret_message = secret_message + (chr(num_list[x]+96))
-     print(secret_message)
